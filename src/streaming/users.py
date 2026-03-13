f6e12a94657912faf3dc8aeafbe71532443260aa
mariasherifnaimfarag <25IMC30128@fh-krems.ac.at>
2026-03-13 19:36:13 +0200

    Update Users.py

--- src/streaming/users.py
+++ src/streaming/users.py
@@ -10,3 +10,64 @@ Classes to implement:
     - FamilyAccountUser
     - FamilyMember
 """
+
+from abc import ABC, abstractmethod
+class User(ABC):
+    def __init__(self,user_id: str,name: str,age: int):
+        self.user_id = user_id
+        self.name = name
+        self.age = age
+        self.List['ListeningSession'] = []
+
+        @abstractmethod
+        def add_session(self,session)->None:
+            pass
+
+        @abstractmethod
+        def total_listening_seconds(self)->int:
+            pass
+
+        @abstractmethod
+        def total_listening_minutes(self)->float:
+            pass
+
+        @abstractmethod
+        def unique_tracks_listened(self)->set[str]:
+            pass
+
+class FreeUser(User):
+    MAX_SKIPS_PER_HOUR=6
+
+class PremiumUser (User):
+    def __init__(self, user_id: str, name: str, age: int,subscription_start:date):
+        super().__init__(user_id,name,age)
+        self.subscription_start= subscription_start
+
+class FamilyAccountUser(User):
+    def __init__(self, user_id: str, name: str, age: int):
+        super().__init__(user_id, name, age)
+        self.sub_users = []
+
+    @abstractmethod
+    def add_sub_user(self,sub_user)->None:
+        pass
+
+    @abstractmethod
+    def all_members(self)-> list:
+        pass
+
+class FamilyMember(User):
+    def __init__(self, user_id: str, name: str, age: int,parent:FamilyAccountUser):
+        super().__init__(user_id, name, age)
+        self.parent = parent
+
+
+
+
+
+
+
+
+
+
+
